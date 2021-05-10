1f9d3f1c06f23ac9746d4d426b88e5a412fea2bd
Evelyn Anyebe <anyebeevelyn@gmail.com>
2021-05-10 17:52:55 +0100

    Add quick sort problem

--- src/quickSort.js
+++ src/quickSort.js
@@ -4,3 +4,27 @@
 // left = all values less than first
 // right = all values greater than first
 // return a combined array [left, first, right]
+function quickSort(arr) {
+  try {
+    if (arr.length < 2) {
+      return arr;
+    }
+    const first = arr[0];
+    let left = [];
+    let right = [];
+
+    for (i=1; i<arr.length; i++) {
+      if (arr[i]< first) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+
+    return [...quickSort(left),first,...quickSort(right)];
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+console.log(quickSort([0,1,2,3,4,5,6,9,10,11,12,8,7]));
